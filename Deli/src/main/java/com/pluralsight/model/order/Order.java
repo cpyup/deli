f904e12e8a0d73a16c1396a6cdc1cdf6a588cfa1
Cameron <cpolly@pgh.yearup.org>
2024-11-12 20:32:05 -0500

    Decoupling Order

--- Deli/src/main/java/com/pluralsight/model/order/Order.java
+++ Deli/src/main/java/com/pluralsight/model/order/Order.java
@@ -1,9 +1,6 @@
 package com.pluralsight.model.order;
 
 import com.pluralsight.interfaces.IGetPrice;
-import com.pluralsight.model.additions.Chips;
-import com.pluralsight.model.additions.Drink;
-import com.pluralsight.model.sandwich.Sandwich;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,13 +23,7 @@ public class Order implements IGetPrice {
     public double getPrice(){
         double totalPrice = 0;
         for(OrderItem item : orderItems){
-            if(item instanceof Sandwich){
-                totalPrice += item.getPrice();
-            }else if(item instanceof Drink){
-                totalPrice += item.getPrice();
-            }else if(item instanceof Chips){
-                totalPrice += item.getPrice();
-            }
+            totalPrice += item.getPrice();
         }
         return totalPrice;
     }
@@ -42,13 +33,7 @@ public class Order implements IGetPrice {
         StringBuilder out = new StringBuilder();
 
         for(OrderItem item : orderItems){
-            if(item instanceof Sandwich){
-                out.append(item).append("\n");
-            }else if(item instanceof Drink){
-                out.append(item).append("\n");
-            }else if(item instanceof Chips){
-                out.append(item).append("\n");
-            }
+            out.append(item).append("\n");
         }
         return out.toString();
     }
